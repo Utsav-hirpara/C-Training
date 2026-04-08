8ac6013fe48df7e23fafb07eaccecc76fd666ed7
Utsav-hirpara <utsav.hirapara@dnkmail.in>
2026-04-08 14:19:05 +0530

    Hello World
    hello : Print Hello World on terminal
    
    Signed-off-by: Utsav-hirpara <utsav.hirapara@dnkmail.in>

--- 01_Hello_World/hello.c
+++ 01_Hello_World/hello.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 
-// void fun()
-// {
-// 	printf("Hello\n");
-// }
-
 int main(void)
 {
 	int ret, num; 
 	char num2;
 	ret = printf("Hello World\n");
 	printf("Ret = %d\n", ret);
-	//fun();
 	ret = scanf("%d %c", &num, &num2);
 	printf("Ret = %d\n", ret);
 
